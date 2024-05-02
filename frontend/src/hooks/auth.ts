93c3563ab809f833955f6f77314e4d482321584d
AkashSDas <aakashdas368@gmail.com>
2024-05-02 20:34:50 +0530

    [fe] create auth hook for creating oauth sessions

--- frontend/src/hooks/auth.ts
+++ frontend/src/hooks/auth.ts
@@ -1,5 +1,9 @@
-import { getNewAccessToken } from "@app/services/auth";
-import { useQuery } from "@tanstack/react-query";
+import { queryClient } from "@app/lib/react-query";
+import { createOAuthSession, getNewAccessToken } from "@app/services/auth";
+import { useToast } from "@chakra-ui/react";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 /**
  * Get currently logged in user's information and update access token
@@ -34,4 +38,68 @@ export function useUser() {
         user: data?.user,
         status,
     };
+}
+
+/**
+ * After successfully being redirected from Google OAuth signup, create a session which
+ * will allow the user to complete the signup process. Allowing user to send authenticated
+ * requests to the server
+ */
+export function useCreateOAuthSession() {
+    const router = useRouter();
+    const toast = useToast();
+
+    const mutation = useMutation({
+        mutationFn: createOAuthSession,
+        onSuccess(data, _varaible, _context) {
+            if (!data.success) {
+                localStorage.removeItem("accessToken");
+                toast({
+                    title: "Failed to create session",
+                    description: data.message,
+                    status: "error",
+                    isClosable: true,
+                });
+            } else if (data.success && data.user) {
+                localStorage.setItem("accessToken", data.accessToken);
+                queryClient.setQueryData(["user"], { user: data.user });
+
+                toast({
+                    title: "Session created",
+                    status: "success",
+                    isClosable: true,
+                });
+
+                // Remove token from url
+                router.replace(router.pathname, undefined, { shallow: true });
+            } else {
+                localStorage.removeItem("accessToken");
+                toast({
+                    title: "Failed to create session",
+                    description: "Please try again later",
+                    status: "error",
+                    isClosable: true,
+                });
+            }
+        },
+        onError(error) {
+            toast({
+                title: "Failed to create session",
+                description: error?.message ?? "Please try again later",
+                status: "error",
+                isClosable: true,
+            });
+        },
+    });
+
+    useEffect(
+        function checkForTokenOnInit(): void {
+            if (typeof router.query?.token === "string") {
+                mutation.mutateAsync(router.query.token);
+            }
+        },
+        [router.query?.token]
+    );
+
+    return { isPending: mutation.isPending, isError: mutation.isError };
 }
