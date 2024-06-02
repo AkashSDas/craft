b8dfcc34af761b99e0ea126b62eae9e4763dfe3e
AkashSDas <aakashdas368@gmail.com>
2024-06-02 16:03:39 +0530

    [fe] refactor navbar

---
+++ frontend/src/components/shared/navbar/MobileNav.tsx
@@ -0,0 +1,170 @@
+import {
+    Show,
+    Button,
+    Drawer,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,
+    DrawerHeader,
+    DrawerBody,
+    VStack,
+    HStack,
+    Divider,
+    Spinner,
+    useDisclosure,
+    Text,
+} from "@chakra-ui/react";
+import Link from "next/link";
+import { Logo } from "../logo";
+import Image from "next/image";
+import { useRef } from "react";
+import { useLogout, useUser } from "@app/hooks/auth";
+
+function ForwardIcon(): React.JSX.Element {
+    return (
+        <Image
+            src={"/icons/direction-right.png"}
+            alt="Move forward"
+            height={28}
+            width={28}
+        />
+    );
+}
+
+export function MobileNav() {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const menuBtnRef = useRef<HTMLButtonElement>(null);
+    const { isPending, logoutUser } = useLogout();
+    const { isLoggedIn } = useUser();
+
+    return (
+        <Show below="sm">
+            <Button
+                ref={menuBtnRef}
+                variant="navPrimary"
+                aria-label={isOpen ? "Close menu" : "Open menu"}
+                px="8px"
+                onClick={onOpen}
+            >
+                <Image
+                    src={isOpen ? "/icons/remove.png" : "/icons/menu.png"}
+                    alt={isOpen ? "Close menu" : "Open menu"}
+                    height={28}
+                    width={28}
+                />
+            </Button>
+
+            {/* Mobile menu drawer */}
+
+            <Drawer
+                isOpen={isOpen}
+                placement="right"
+                onClose={onClose}
+                size="md"
+                finalFocusRef={menuBtnRef}
+            >
+                <DrawerOverlay />
+
+                <DrawerContent>
+                    <DrawerCloseButton
+                        as={Button}
+                        variant="paleSolid"
+                        sx={{
+                            _active: { bgColor: "white" },
+                            _hover: { bgColor: "white" },
+                        }}
+                    />
+
+                    <DrawerHeader>
+                        <Logo variant="light" />
+                    </DrawerHeader>
+
+                    <DrawerBody>
+                        <VStack gap="0.5rem">
+                            <HStack
+                                w="100%"
+                                px="8px"
+                                py="12px"
+                                justifyContent="space-between"
+                                borderRadius="4px"
+                                cursor="pointer"
+                                _hover={{ bgColor: "gray.100" }}
+                                _active={{ bgColor: "gray.200" }}
+                                onClick={onClose}
+                            >
+                                <Text
+                                    fontWeight="600"
+                                    fontSize="24px"
+                                    color="gray.700"
+                                >
+                                    About
+                                </Text>
+                                <ForwardIcon />
+                            </HStack>
+
+                            <Divider />
+
+                            {isLoggedIn ? (
+                                <Button
+                                    variant="paleSolid"
+                                    onClick={logoutUser}
+                                    disabled={isPending}
+                                    isDisabled={isPending}
+                                    fontSize="1rem"
+                                    w="100%"
+                                    mt="1rem"
+                                >
+                                    {isPending ? (
+                                        <Spinner size="xs" />
+                                    ) : (
+                                        "Logout"
+                                    )}
+                                </Button>
+                            ) : (
+                                <>
+                                    <HStack
+                                        w="100%"
+                                        px="8px"
+                                        py="12px"
+                                        justifyContent="space-between"
+                                        borderRadius="4px"
+                                        _hover={{ bgColor: "gray.100" }}
+                                        _active={{
+                                            bgColor: "gray.200",
+                                        }}
+                                        as={Link}
+                                        href="/auth/login"
+                                        onClick={onClose}
+                                    >
+                                        <Text
+                                            fontWeight="600"
+                                            fontSize="24px"
+                                            color="gray.700"
+                                        >
+                                            Login
+                                        </Text>
+                                        <ForwardIcon />
+                                    </HStack>
+
+                                    <Divider />
+
+                                    <Button
+                                        variant="solid"
+                                        w="100%"
+                                        mt="1rem"
+                                        as={Link}
+                                        href="/auth/signup"
+                                        onClick={onClose}
+                                        fontSize="1rem"
+                                    >
+                                        Signup
+                                    </Button>
+                                </>
+                            )}
+                        </VStack>
+                    </DrawerBody>
+                </DrawerContent>
+            </Drawer>
+        </Show>
+    );
+}
--- frontend/src/components/shared/navbar/Navbar.tsx
+++ frontend/src/components/shared/navbar/Navbar.tsx
@@ -1,40 +1,10 @@
-import {
-    Button,
-    Divider,
-    Drawer,
-    DrawerBody,
-    DrawerCloseButton,
-    DrawerContent,
-    DrawerHeader,
-    DrawerOverlay,
-    HStack,
-    Show,
-    Spinner,
-    Text,
-    VStack,
-    useDisclosure,
-} from "@chakra-ui/react";
+import { Button, HStack, Show, Spinner } from "@chakra-ui/react";
 import Link from "next/link";
 import { Logo } from "../logo";
-import Image from "next/image";
-import { useRef } from "react";
 import { useLogout, useUser } from "@app/hooks/auth";
-
-function ForwardIcon(): React.JSX.Element {
-    return (
-        <Image
-            src={"/icons/direction-right.png"}
-            alt="Move forward"
-            height={28}
-            width={28}
-        />
-    );
-}
+import { MobileNav } from "./MobileNav";
 
 export function Navbar() {
-    const { isOpen, onOpen, onClose } = useDisclosure();
-    const menuBtnRef = useRef<HTMLButtonElement>(null);
-    const { isPending, logoutUser } = useLogout();
     const { isLoggedIn } = useUser();
 
     return (
@@ -50,174 +20,49 @@ export function Navbar() {
             </Link>
 
             <HStack gap="24px">
-                {/* Large screen nav */}
-
                 <Show above="sm">
-                    <Button variant="navItem">Write</Button>
-
-                    {isLoggedIn ? (
-                        <Button
-                            variant="navItem"
-                            onClick={logoutUser}
-                            disabled={isPending}
-                            isDisabled={isPending}
-                        >
-                            {isPending ? <Spinner size="xs" /> : "Logout"}
-                        </Button>
-                    ) : (
-                        <>
-                            <Button
-                                as={Link}
-                                href="/auth/login"
-                                variant="navItem"
-                            >
-                                Login
-                            </Button>
-
-                            <Button
-                                as={Link}
-                                href="/auth/signup"
-                                variant="navPrimary"
-                            >
-                                Get Started
-                            </Button>
-                        </>
-                    )}
+                    {isLoggedIn ? <LoggedInItems /> : <LoggOutItems />}
                 </Show>
 
-                {/* Mobile menu */}
-
-                <Show below="sm">
-                    <Button
-                        ref={menuBtnRef}
-                        variant="navPrimary"
-                        aria-label={isOpen ? "Close menu" : "Open menu"}
-                        px="8px"
-                        onClick={onOpen}
-                    >
-                        <Image
-                            src={
-                                isOpen ? "/icons/remove.png" : "/icons/menu.png"
-                            }
-                            alt={isOpen ? "Close menu" : "Open menu"}
-                            height={28}
-                            width={28}
-                        />
-                    </Button>
-
-                    {/* Mobile menu drawer */}
-
-                    <Drawer
-                        isOpen={isOpen}
-                        placement="right"
-                        onClose={onClose}
-                        size="md"
-                        finalFocusRef={menuBtnRef}
-                    >
-                        <DrawerOverlay />
-
-                        <DrawerContent>
-                            <DrawerCloseButton
-                                as={Button}
-                                variant="paleSolid"
-                                sx={{
-                                    _active: { bgColor: "white" },
-                                    _hover: { bgColor: "white" },
-                                }}
-                            />
-
-                            <DrawerHeader>
-                                <Logo variant="light" />
-                            </DrawerHeader>
-
-                            <DrawerBody>
-                                <VStack gap="0.5rem">
-                                    <HStack
-                                        w="100%"
-                                        px="8px"
-                                        py="12px"
-                                        justifyContent="space-between"
-                                        borderRadius="4px"
-                                        cursor="pointer"
-                                        _hover={{ bgColor: "gray.100" }}
-                                        _active={{ bgColor: "gray.200" }}
-                                        onClick={onClose}
-                                    >
-                                        <Text
-                                            fontWeight="600"
-                                            fontSize="24px"
-                                            color="gray.700"
-                                        >
-                                            About
-                                        </Text>
-                                        <ForwardIcon />
-                                    </HStack>
-
-                                    <Divider />
-
-                                    {isLoggedIn ? (
-                                        <Button
-                                            variant="paleSolid"
-                                            onClick={logoutUser}
-                                            disabled={isPending}
-                                            isDisabled={isPending}
-                                            fontSize="1rem"
-                                            w="100%"
-                                            mt="1rem"
-                                        >
-                                            {isPending ? (
-                                                <Spinner size="xs" />
-                                            ) : (
-                                                "Logout"
-                                            )}
-                                        </Button>
-                                    ) : (
-                                        <>
-                                            <HStack
-                                                w="100%"
-                                                px="8px"
-                                                py="12px"
-                                                justifyContent="space-between"
-                                                borderRadius="4px"
-                                                _hover={{ bgColor: "gray.100" }}
-                                                _active={{
-                                                    bgColor: "gray.200",
-                                                }}
-                                                as={Link}
-                                                href="/auth/login"
-                                                onClick={onClose}
-                                            >
-                                                <Text
-                                                    fontWeight="600"
-                                                    fontSize="24px"
-                                                    color="gray.700"
-                                                >
-                                                    Login
-                                                </Text>
-                                                <ForwardIcon />
-                                            </HStack>
-
-                                            <Divider />
-
-                                            <Button
-                                                variant="solid"
-                                                w="100%"
-                                                mt="1rem"
-                                                as={Link}
-                                                href="/auth/signup"
-                                                onClick={onClose}
-                                                fontSize="1rem"
-                                            >
-                                                Signup
-                                            </Button>
-                                        </>
-                                    )}
-                                </VStack>
-                            </DrawerBody>
-                        </DrawerContent>
-                    </Drawer>
-                </Show>
+                <MobileNav />
             </HStack>
         </HStack>
+    );
+}
+
+function LoggedInItems() {
+    const { isPending, logoutUser } = useLogout();
+
+    return (
+        <>
+            <Button variant="navItem" as={Link} href="/editor">
+                Write
+            </Button>
+
+            <Button
+                variant="navItem"
+                onClick={logoutUser}
+                disabled={isPending}
+                isDisabled={isPending}
+            >
+                {isPending ? <Spinner size="xs" /> : "Logout"}
+            </Button>
+        </>
+    );
+}
+
+function LoggOutItems() {
+    return (
+        <>
+            <>
+                <Button as={Link} href="/auth/login" variant="navItem">
+                    Login
+                </Button>
+
+                <Button as={Link} href="/auth/signup" variant="navPrimary">
+                    Get Started
+                </Button>
+            </>
+        </>
     );
 }
