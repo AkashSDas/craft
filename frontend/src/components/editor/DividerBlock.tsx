1bccc21d708a92e99c7c2a61cbf64a4822ade6ab
AkashSDas <aakashdas368@gmail.com>
2024-06-03 19:39:21 +0530

    [fe] create divider and image blocks

---
+++ frontend/src/components/editor/DividerBlock.tsx
@@ -0,0 +1,78 @@
+import {
+    Divider,
+    IconButton,
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuList,
+    Stack,
+} from "@chakra-ui/react";
+import { useAppDispatch } from "@app/hooks/store";
+import { useState } from "react";
+import { deleteBlock } from "@app/store/editor/slice";
+import { DeleteIcon, DragHandleIcon } from "@chakra-ui/icons";
+
+type Props = {
+    blockId: string;
+};
+
+export function DividerBlock(props: Props) {
+    const { blockId } = props;
+    const dispatch = useAppDispatch();
+    const [isMenuOpen, setMenuOpen] = useState(false);
+
+    return (
+        <Stack pos="relative" role="group" mt="1rem">
+            <Menu onClose={() => setMenuOpen(false)}>
+                <MenuButton
+                    as={IconButton}
+                    opacity={isMenuOpen ? 1 : 0}
+                    transition="opacity 0.1.5s ease-in-out"
+                    _groupHover={{ opacity: 1 }}
+                    width="fit-content"
+                    variant="paleSolid"
+                    onClick={() => setMenuOpen(true)}
+                    aria-label="Drag"
+                    fontSize="smaller"
+                    pos="absolute"
+                    left="-32px"
+                    top="30%"
+                    h="28px"
+                    w="24px"
+                    p="0"
+                    minW="22px"
+                    maxW="22px"
+                >
+                    <DragHandleIcon color="gray" />
+                </MenuButton>
+
+                <MenuList
+                    bgColor="white"
+                    border="1.5px solid"
+                    mt="12px"
+                    py="8px"
+                    px="0px"
+                    borderColor="gray.300"
+                    borderRadius="4px"
+                    boxShadow="0px 4px 8px rgba(57, 57, 57, 0.25)"
+                >
+                    <MenuItem
+                        h="36px"
+                        borderRadius="4px"
+                        fontSize="14px"
+                        color="gray.400"
+                        fontWeight="medium"
+                        _hover={{ bgColor: "gray.100", color: "red.500" }}
+                        _active={{ bgColor: "gray.200" }}
+                        onClick={() => dispatch(deleteBlock(blockId))}
+                        icon={<DeleteIcon fontSize="medium" />}
+                    >
+                        Delete
+                    </MenuItem>
+                </MenuList>
+            </Menu>
+
+            <Divider my="2rem" />
+        </Stack>
+    );
+}
---
+++ frontend/src/components/editor/ImageBlock.tsx
@@ -0,0 +1,128 @@
+import {
+    Box,
+    Button,
+    Divider,
+    IconButton,
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuList,
+    Stack,
+    StackDivider,
+    Text,
+} from "@chakra-ui/react";
+import { useAppDispatch, useAppSelector } from "@app/hooks/store";
+import { ChangeEvent, useRef, useState } from "react";
+import {
+    deleteBlock,
+    selectBlock,
+    selectFile,
+    updateImageBlock,
+} from "@app/store/editor/slice";
+import { AddIcon, DeleteIcon, DragHandleIcon } from "@chakra-ui/icons";
+import { Image } from "@app/services/articles";
+import NextImage from "next/image";
+
+type Props = {
+    blockId: string;
+};
+
+export function ImageBlock(props: Props) {
+    const { blockId } = props;
+    const block = useAppSelector((s) => selectBlock(s, blockId) as Image);
+    const file = useAppSelector((s) => selectFile(s, block.blockId));
+    const dispatch = useAppDispatch();
+    const [isMenuOpen, setMenuOpen] = useState(false);
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    function handleFileChange(e: ChangeEvent<HTMLInputElement>) {
+        const file = e.target.files?.[0];
+        if (file) {
+            dispatch(updateImageBlock({ blockId, file }));
+        }
+    }
+
+    return (
+        <Stack pos="relative" role="group" mt="1rem">
+            <Menu onClose={() => setMenuOpen(false)}>
+                <MenuButton
+                    as={IconButton}
+                    opacity={isMenuOpen ? 1 : 0}
+                    transition="opacity 0.1.5s ease-in-out"
+                    _groupHover={{ opacity: 1 }}
+                    width="fit-content"
+                    variant="paleSolid"
+                    onClick={() => setMenuOpen(true)}
+                    aria-label="Drag"
+                    fontSize="smaller"
+                    pos="absolute"
+                    left="-32px"
+                    top="6px"
+                    h="28px"
+                    w="24px"
+                    p="0"
+                    minW="22px"
+                    maxW="22px"
+                >
+                    <DragHandleIcon color="gray" />
+                </MenuButton>
+
+                <MenuList
+                    bgColor="white"
+                    border="1.5px solid"
+                    mt="12px"
+                    py="8px"
+                    px="0px"
+                    borderColor="gray.300"
+                    borderRadius="4px"
+                    boxShadow="0px 4px 8px rgba(57, 57, 57, 0.25)"
+                >
+                    <MenuItem
+                        h="36px"
+                        borderRadius="4px"
+                        fontSize="14px"
+                        color="gray.400"
+                        fontWeight="medium"
+                        _hover={{ bgColor: "gray.100", color: "red.500" }}
+                        _active={{ bgColor: "gray.200" }}
+                        onClick={() => dispatch(deleteBlock(blockId))}
+                        icon={<DeleteIcon fontSize="medium" />}
+                    >
+                        Delete
+                    </MenuItem>
+                </MenuList>
+            </Menu>
+
+            {!file ? (
+                <Button
+                    variant="outline"
+                    leftIcon={<AddIcon />}
+                    color="gray"
+                    onClick={() => inputRef.current?.click()}
+                >
+                    <Text>Add Image</Text>
+                </Button>
+            ) : (
+                <Box pos="relative" height="300px" width="100%">
+                    <NextImage
+                        src={block.value.URL ?? ""}
+                        alt="Image"
+                        layout="fill"
+                        style={{
+                            borderRadius: "4px",
+                            objectFit: "cover",
+                            border: "1.5px solid black",
+                        }}
+                    />
+                </Box>
+            )}
+
+            <input
+                type="file"
+                ref={inputRef}
+                style={{ display: "none" }}
+                onChange={handleFileChange}
+            />
+        </Stack>
+    );
+}
