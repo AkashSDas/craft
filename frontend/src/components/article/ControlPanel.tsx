fc8d41d3e7335d1f59aa447a4fd621820cec1b9d
AkashSDas <aakashdas368@gmail.com>
2024-06-07 23:51:50 +0530

    [fe] create control panel component

---
+++ frontend/src/components/article/ControlPanel.tsx
@@ -0,0 +1,73 @@
+import { HStack, Button } from "@chakra-ui/react";
+import Image from "next/image";
+
+export function ControlPanel() {
+    return (
+        <HStack
+            justifyContent="space-between"
+            mt="1.5rem"
+            py="8px"
+            w="100%"
+            borderY="1px solid"
+            borderColor="gray.300"
+        >
+            <HStack>
+                <Button h="38px" variant="tab">
+                    <Image
+                        src="/icons/play.png"
+                        alt="Play audio"
+                        width={20}
+                        height={20}
+                    />
+                </Button>
+
+                <Button h="38px" variant="tab">
+                    <Image
+                        src="/icons/open-book.png"
+                        alt="Get summary"
+                        width={20}
+                        height={20}
+                    />
+                </Button>
+            </HStack>
+
+            <HStack>
+                <Button h="38px" variant="tab">
+                    <Image
+                        src="/icons/love.png"
+                        alt="Like"
+                        width={20}
+                        height={20}
+                    />
+                </Button>
+
+                <Button h="38px" variant="tab">
+                    <Image
+                        src="/icons/chat.png"
+                        alt="Comments"
+                        width={20}
+                        height={20}
+                    />
+                </Button>
+
+                <Button h="38px" variant="tab">
+                    <Image
+                        src="/icons/bookmark.png"
+                        alt="Save"
+                        width={20}
+                        height={20}
+                    />
+                </Button>
+
+                <Button h="38px" variant="tab">
+                    <Image
+                        src="/icons/share.png"
+                        alt="Share"
+                        width={20}
+                        height={20}
+                    />
+                </Button>
+            </HStack>
+        </HStack>
+    );
+}
