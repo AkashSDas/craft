0c0769e2cb9954f8b8155a78d494aae13c49ea80
AkashSDas <aakashdas368@gmail.com>
2024-06-09 14:56:23 +0530

    [be] add methods for likes repository

--- backend/src/likes/likes.repository.ts
+++ backend/src/likes/likes.repository.ts
@@ -1,4 +1,39 @@
 import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Like } from "./schema";
+import { Model, Types } from "mongoose";
 
 @Injectable()
-export class LikesRepository {}
+export class LikesRepository {
+    constructor(@InjectModel(Like.name) private model: Model<Like>) {}
+
+    async createLike(articleId: Types.ObjectId, userId: Types.ObjectId) {
+        return await this.model.create({
+            articleId,
+            userId,
+        });
+    }
+
+    async exists(articleId: Types.ObjectId, userId: Types.ObjectId) {
+        return await this.model.exists({ articleId, userId });
+    }
+
+    async deleteLike(
+        articleId: Types.ObjectId,
+        userId: Types.ObjectId,
+    ): Promise<any> {
+        return await this.model.deleteOne({ articleId, userId });
+    }
+
+    async getLikes(articleId: Types.ObjectId) {
+        return await this.model.find({ articleId });
+    }
+
+    async getLikesCount(articleId: Types.ObjectId) {
+        return await this.model.find({ articleId }).countDocuments();
+    }
+
+    async getUserLikes(userId: Types.ObjectId) {
+        return await this.model.findOne({ userId });
+    }
+}
