cc0d51df26f49532ffbca668d2b279dd1e3a7642
AkashSDas <aakashdas368@gmail.com>
2024-06-09 12:49:09 +0530

    [be] create methods for CRUD in followers repository

--- backend/src/followers/followers.repository.ts
+++ backend/src/followers/followers.repository.ts
@@ -1,4 +1,37 @@
 import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Follower } from "./schema";
+import { Model } from "mongoose";
+
+// followerId -- the user who is following
+// followingId -- the user who is being followed
 
 @Injectable()
-export class FollowersRepository {}
+export class FollowersRepository {
+    constructor(@InjectModel(Follower.name) private model: Model<Follower>) {}
+
+    async createFollower(followerId: string, followingId: string) {
+        return await this.model.create({
+            followerId,
+            followingId,
+        });
+    }
+
+    async deleteFollower(
+        followerId: string,
+        followingId: string,
+    ): Promise<any> {
+        return await this.model.deleteOne({
+            followerId,
+            followingId,
+        });
+    }
+
+    async getFollowers(followingId: string) {
+        return await this.model.find({ followingId });
+    }
+
+    async getFollowing(followerId: string) {
+        return await this.model.find({ followerId });
+    }
+}
