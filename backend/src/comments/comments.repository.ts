148b1621e8c98cc1cd9de5e1f6fb259584a6387c
AkashSDas <aakashdas368@gmail.com>
2024-06-10 07:52:05 +0530

    [be] update comment schema and add methods on comment repo

--- backend/src/comments/comments.repository.ts
+++ backend/src/comments/comments.repository.ts
@@ -1,6 +1,33 @@
 import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model, Types } from "mongoose";
+import { Comment } from "./schema";
 
 @Injectable()
 export class CommentRepository {
-    // constructor(@InjectModel(Comment.name) private model: Model<Comment>) {}
+    constructor(@InjectModel(Comment.name) private model: Model<Comment>) {}
+
+    async createComment(comment: Partial<Comment>) {
+        return this.model.create(comment);
+    }
+
+    async deleteComment(commentId: Types.ObjectId) {
+        return this.model.findByIdAndDelete(commentId);
+    }
+
+    async deleteChildComments(parentCommentId: Types.ObjectId): Promise<any> {
+        return this.model.deleteMany({ parentCommentId });
+    }
+
+    async getCommentsByArticleId(articleId: Types.ObjectId) {
+        return this.model.find({ articleId });
+    }
+
+    async getChildComments(parentCommentId: Types.ObjectId) {
+        return this.model.find({ parentCommentId });
+    }
+
+    async updateCommentText(commentId: Types.ObjectId, text: string) {
+        return this.model.findByIdAndUpdate(commentId, { text });
+    }
 }
--- backend/src/comments/schema/comment.schema.ts
+++ backend/src/comments/schema/comment.schema.ts
@@ -1,11 +1,13 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { SchemaTypes, Types } from "mongoose";
+import { Article } from "src/articles/schema";
 import { User } from "src/users/schema";
 
 export interface IComment {
     text: string;
     reportCount: number;
     authorId: Types.ObjectId;
+    articleId: Types.ObjectId;
     parentCommentId?: Types.ObjectId;
 }
 
@@ -13,6 +15,9 @@ export interface IComment {
 export class Comment extends Document implements IComment {
     @Prop({ type: SchemaTypes.ObjectId, required: true, ref: User.name })
     authorId: Types.ObjectId;
+
+    @Prop({ type: SchemaTypes.ObjectId, required: true, ref: Article.name })
+    articleId: Types.ObjectId;
 
     @Prop({ type: String, trim: true, required: true })
     text: string;
