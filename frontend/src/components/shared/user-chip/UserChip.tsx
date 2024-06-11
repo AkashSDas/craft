93a6d74c2f8ac81b7cffb4947e0bc43c3612cee5
AkashSDas <aakashdas368@gmail.com>
2024-06-11 21:12:24 +0530

    [fe] create UserChip component

---
+++ frontend/src/components/shared/user-chip/UserChip.tsx
@@ -0,0 +1,43 @@
+import { HStack, Text } from "@chakra-ui/react";
+import Image from "next/image";
+import Link from "next/link";
+
+type Props = {
+    alt: string;
+    avatarURL?: string | null;
+    username: string;
+    userId: string;
+};
+
+export function UserChip(props: Props): React.JSX.Element {
+    const { alt, avatarURL, username } = props;
+
+    return (
+        <HStack
+            bgColor="gray.100"
+            borderRadius="50px"
+            py="2px"
+            pl="3px"
+            pr="12px"
+            as={Link}
+            href={`/authors/${props.userId}`}
+        >
+            <Image
+                src={avatarURL ?? "/mascot.png"}
+                alt={alt}
+                height={24}
+                width={24}
+                style={{
+                    border: "2px solid",
+                    borderColor: "#B36439",
+                    objectFit: "cover",
+                    borderRadius: "50%",
+                }}
+            />
+
+            <Text fontSize="sm" fontWeight="medium">
+                {username}
+            </Text>
+        </HStack>
+    );
+}
