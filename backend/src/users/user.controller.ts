2317f4d94b8a255bf6e542d448b5dd565eb565d3
AkashSDas <aakashdas368@gmail.com>
2024-06-18 09:35:01 +0530

    [be] fix get author endpoint

--- backend/src/users/user.controller.ts
+++ backend/src/users/user.controller.ts
@@ -1,12 +1,20 @@
 import { Controller, Get, Param } from "@nestjs/common";
 import { UserService } from "./user.service";
+import { NotFoundException } from "@nestjs/common";
 
-@Controller("articles")
+@Controller("users")
 export class UserController {
     constructor(private serv: UserService) {}
 
     @Get("authors/:authorId")
-    getAuthorProfile(@Param("authorId") authorId: string) {
-        return this.serv.getAuthorProfile(authorId);
+    async getAuthorProfile(@Param("authorId") authorId: string) {
+        const author = await this.serv.getAuthorProfile(authorId);
+        console.log(author);
+
+        if (!author) {
+            throw new NotFoundException("Author not found");
+        }
+
+        return { author };
     }
 }
