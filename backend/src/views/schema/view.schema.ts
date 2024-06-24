54f0299dd8787f1c0b5bcf35a6b8cdf28e67db7c
AkashSDas <aakashdas368@gmail.com>
2024-06-24 20:53:33 +0530

    [be] create views schema

---
+++ backend/src/views/schema/view.schema.ts
@@ -0,0 +1,28 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document, SchemaTypes, Types } from "mongoose";
+import { Article } from "src/articles/schema";
+import { User } from "src/users/schema";
+
+export interface IView {
+    articleId: Types.ObjectId;
+    userId: Types.ObjectId;
+    readTimeInMs: number;
+    ipAddress: string;
+}
+
+@Schema({ timestamps: true })
+export class View extends Document implements IView {
+    @Prop({ type: SchemaTypes.ObjectId, required: true, ref: Article.name })
+    articleId: Types.ObjectId;
+
+    @Prop({ type: SchemaTypes.ObjectId, required: true, ref: User.name })
+    userId: Types.ObjectId;
+
+    @Prop({ type: Number, required: true, default: 0 })
+    readTimeInMs: number;
+
+    @Prop({ type: String, required: true, select: false })
+    ipAddress: string;
+}
+
+export const ViewSchema = SchemaFactory.createForClass(View);
