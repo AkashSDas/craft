2242ea70ea9821c83d95d730666929e459d435be
AkashSDas <aakashdas368@gmail.com>
2024-06-25 19:11:49 +0530

    [fe] show recent searches in search input

--- frontend/src/components/search/SearchButton.tsx
+++ frontend/src/components/search/SearchButton.tsx
@@ -1,16 +1,23 @@
 import { useRecentSearch } from "@app/hooks/search";
+import { DeleteIcon } from "@chakra-ui/icons";
 import {
+    Box,
+    Divider,
+    HStack,
     IconButton,
     Input,
     InputGroup,
     InputRightElement,
+    Text,
+    VStack,
     useMediaQuery,
+    useOutsideClick,
     useQuery,
 } from "@chakra-ui/react";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { FormEvent, useEffect, useState } from "react";
+import { FormEvent, useEffect, useRef, useState } from "react";
 
 export function SearchButton() {
     const query = useQuery({ above: "md" });
@@ -20,7 +27,13 @@ export function SearchButton() {
     });
     const [searchText, setSearchText] = useState("");
     const router = useRouter();
-    const { addHistory } = useRecentSearch();
+    const { addHistory, history, removeHistory } = useRecentSearch();
+    const [showHistory, setShowHistory] = useState(false);
+    const ref = useRef<HTMLDivElement>(null);
+    useOutsideClick({
+        ref: ref,
+        handler: () => setShowHistory(false),
+    });
 
     function handleSubmit(e: FormEvent<HTMLDivElement>) {
         e.preventDefault();
@@ -45,51 +58,135 @@ export function SearchButton() {
 
     if (isAboveSm) {
         return (
-            <InputGroup as="form" onSubmit={handleSubmit}>
-                <Input
-                    placeholder="Search"
-                    variant="outline"
-                    _focusVisible={{ outline: "none" }}
-                    value={searchText}
-                    onChange={(e) => setSearchText(e.target.value)}
-                    sx={{
-                        h: "38px",
-                        borderColor: "gray.600",
-                        color: "gray.200",
-                        fontWeight: "500",
-                        "&::placeholder": {
-                            color: "gray.500",
-                            fontWeight: "500",
-                        },
-                        _focus: {
-                            border: "1.5px solid",
-                            borderColor: "gray.400",
-                        },
-                        _hover: {
-                            border: "1.5px solid",
-                            borderColor: "gray.500",
-                        },
-                    }}
-                />
-
-                <InputRightElement maxH="38px">
-                    <IconButton
-                        type="submit"
-                        aria-label="Search"
-                        variant="navItem"
+            <Box pos="relative" ref={ref}>
+                <InputGroup as="form" onSubmit={handleSubmit}>
+                    <Input
+                        placeholder="Search"
+                        variant="outline"
+                        _focusVisible={{ outline: "none" }}
+                        value={searchText}
+                        onChange={(e) => setSearchText(e.target.value)}
+                        onFocus={() => setShowHistory(true)}
                         sx={{
-                            maxH: "30px !important",
+                            h: "38px",
+                            borderColor: "gray.600",
+                            color: "gray.200",
+                            fontWeight: "500",
+                            "&::placeholder": {
+                                color: "gray.500",
+                                fontWeight: "500",
+                            },
+                            _focus: {
+                                border: "1.5px solid",
+                                borderColor: "gray.400",
+                            },
+                            _hover: {
+                                border: "1.5px solid",
+                                borderColor: "gray.500",
+                            },
                         }}
+                    />
+
+                    <InputRightElement maxH="38px">
+                        <IconButton
+                            type="submit"
+                            aria-label="Search"
+                            variant="navItem"
+                            sx={{ maxH: "30px !important" }}
+                            onClick={() => {
+                                setShowHistory(false);
+                            }}
+                        >
+                            <Image
+                                src="/icons/search-light.png"
+                                alt="Search"
+                                width={16}
+                                height={16}
+                            />
+                        </IconButton>
+                    </InputRightElement>
+                </InputGroup>
+
+                {showHistory && history.length > 0 ? (
+                    <VStack
+                        w="400px"
+                        border="1px solid"
+                        borderColor="gray.200"
+                        borderRadius="4px"
+                        bgColor="gray.50"
+                        boxShadow="0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
+                        pos="absolute"
+                        top="44px"
+                        right="0"
+                        py="8px"
+                        px="8px"
+                        alignItems="start"
                     >
-                        <Image
-                            src="/icons/search-light.png"
-                            alt="Search"
-                            width={16}
-                            height={16}
-                        />
-                    </IconButton>
-                </InputRightElement>
-            </InputGroup>
+                        <Text
+                            fontSize="14px"
+                            color="gray.500"
+                            fontWeight="500"
+                            pl="8px"
+                        >
+                            Recent Searches
+                        </Text>
+
+                        <Divider borderColor="gray.200" />
+
+                        {history.map((item) => {
+                            const params = new URLSearchParams({ q: item });
+
+                            return (
+                                <HStack
+                                    w="100%"
+                                    key={item}
+                                    h="40px"
+                                    borderRadius="4px"
+                                    as={Link}
+                                    onClick={() => {
+                                        setSearchText(item);
+                                        setShowHistory(false);
+                                    }}
+                                    href={`/search?${params.toString()}`}
+                                    _hover={{ bgColor: "gray.100" }}
+                                    transition="transform .3s cubic-bezier(.5,2.5,.7,.7),-webkit-transform .3s cubic-bezier(.5,2.5,.7,.7)"
+                                    transformOrigin="center"
+                                    _active={{
+                                        bgColor: "gray.200",
+                                        transform: "scale(0.98)",
+                                    }}
+                                    px="12px"
+                                >
+                                    <Text
+                                        flexGrow={1}
+                                        fontSize="14px"
+                                        color="gray.600"
+                                        fontWeight="500"
+                                        noOfLines={1}
+                                    >
+                                        {item}
+                                    </Text>
+
+                                    <IconButton
+                                        aria-label="Delete"
+                                        variant="tab"
+                                        h="28px"
+                                        transition="all 0.2s ease-in-out"
+                                        onClick={(e) => {
+                                            e.preventDefault();
+                                            e.stopPropagation();
+                                            removeHistory(item);
+                                            setShowHistory(false);
+                                        }}
+                                    >
+                                        <DeleteIcon />
+                                    </IconButton>
+                                </HStack>
+                            );
+                        })}
+                    </VStack>
+                ) : null}
+            </Box>
         );
     } else {
         return (
