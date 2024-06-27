e86902e77e8ba0696a258ca9da0543ebfa613456
AkashSDas <aakashdas368@gmail.com>
2024-06-27 23:28:51 +0530

    [be] fix sorting and query in article stats

--- backend/src/views/views.repository.ts
+++ backend/src/views/views.repository.ts
@@ -3,10 +3,15 @@ import { InjectModel } from "@nestjs/mongoose";
 import { View } from "./schema";
 import { FilterQuery, Model, Types, UpdateQuery } from "mongoose";
 import { LifetimeViewsQuery } from "./query";
+import { ArticleService } from "src/articles/article.service";
+import { ArticleRepository } from "src/articles/article.repository";
 
 @Injectable()
 export class ViewsRepsitory {
-    constructor(@InjectModel(View.name) private model: Model<View>) {}
+    constructor(
+        @InjectModel(View.name) private model: Model<View>,
+        private articleRepo: ArticleRepository,
+    ) {}
 
     async create(view: Partial<View>) {
         return await this.model.create(view);
@@ -82,52 +87,19 @@ export class ViewsRepsitory {
         // and sort them on the basis of query.filterProperty
 
         const { filterProperty, order } = query;
-        let sort: Record<string, 1 | 0> = { "articleInfo.lastUpdatedAt": 1 };
+        let sort: Record<string, 1 | -1> = { "articleInfo.lastUpdatedAt": 1 };
 
         switch (filterProperty) {
             case "lastUpdatedAt":
-                sort = { "articleInfo.lastUpdatedAt": order === "ASC" ? 0 : 1 };
+                sort = {
+                    "articleInfo.lastUpdatedAt": order === "ASC" ? -1 : 1,
+                };
             case "readCount":
-                sort = { totalReads: order === "ASC" ? 0 : 1 };
+                sort = { totalReads: order === "ASC" ? 1 : -1 };
             case "viewCount":
-                sort = { totalViews: order === "ASC" ? 0 : 1 };
+                sort = { totalViews: order === "ASC" ? 1 : -1 };
         }
 
-        const results = this.model.aggregate([
-            { $match: { userId } },
-            {
-                $group: {
-                    _id: "$articleId",
-                    totalViews: { $sum: 1 },
-                    totalReadTimeInMs: { $sum: "$readTimeInMs" },
-                    totalReads: {
-                        $sum: {
-                            $cond: [{ $eq: ["$hasRead", true] }, 1, 0],
-                        },
-                    },
-                },
-            },
-            {
-                $lookup: {
-                    from: "articles",
-                    localField: "_id",
-                    foreignField: "_id",
-                    as: "articleInfo",
-                },
-            },
-            {
-                $project: {
-                    articleId: "$_id",
-                    totalViews: 1,
-                    totalReadTimeInMs: 1,
-                    totalReads: 1,
-                    articleInfo: 1,
-                    _id: 0,
-                },
-            },
-            { $sort: sort as any },
-        ]);
-
-        return results;
+        return this.articleRepo.getUserArticlesLifetimeStats(userId, sort);
     }
 }
