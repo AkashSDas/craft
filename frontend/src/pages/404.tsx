51b18862658523cfac4bcd77ac841df07ead8002
AkashSDas <aakashdas368@gmail.com>
2024-08-11 20:08:16 +0530

    add title in 404 and 500 page

--- frontend/src/pages/404.tsx
+++ frontend/src/pages/404.tsx
@@ -1,19 +1,27 @@
 import { Layout } from "@app/components/shared/layout/Layout";
 import { fontStyles } from "@app/utils/fonts";
 import { Text } from "@chakra-ui/react";
+import Head from "next/head";
 
 export default function NotFoundPage() {
     return (
-        <Layout>
-            <Text
-                data-testid="not-found-page-text"
-                fontWeight="500"
-                color="gray.600"
-                fontSize="20px"
-                fontFamily={fontStyles["expandedBoldItalic"].fontFamily}
-            >
-                Resource not found
-            </Text>
-        </Layout>
+        <>
+            <Head>
+                <title>Not Found</title>
+                <meta name="description" content="Resource not found" />
+            </Head>
+
+            <Layout>
+                <Text
+                    data-testid="not-found-page-text"
+                    fontWeight="500"
+                    color="gray.600"
+                    fontSize="20px"
+                    fontFamily={fontStyles["expandedBoldItalic"].fontFamily}
+                >
+                    Resource not found
+                </Text>
+            </Layout>
+        </>
     );
 }
--- frontend/src/pages/500.tsx
+++ frontend/src/pages/500.tsx
@@ -1,19 +1,27 @@
 import { Layout } from "@app/components/shared/layout/Layout";
 import { fontStyles } from "@app/utils/fonts";
 import { Text } from "@chakra-ui/react";
+import Head from "next/head";
 
 export default function InternalServerErrorPage() {
     return (
-        <Layout>
-            <Text
-                data-testid="internal-server-error-page-text"
-                fontWeight="500"
-                color="gray.600"
-                fontSize="20px"
-                fontFamily={fontStyles["expandedBoldItalic"].fontFamily}
-            >
-                Internal Server Error
-            </Text>
-        </Layout>
+        <>
+            <Head>
+                <title>Internal Server Error</title>
+                <meta name="description" content="Internal Server Error" />
+            </Head>
+
+            <Layout>
+                <Text
+                    data-testid="internal-server-error-page-text"
+                    fontWeight="500"
+                    color="gray.600"
+                    fontSize="20px"
+                    fontFamily={fontStyles["expandedBoldItalic"].fontFamily}
+                >
+                    Internal Server Error
+                </Text>
+            </Layout>
+        </>
     );
 }
